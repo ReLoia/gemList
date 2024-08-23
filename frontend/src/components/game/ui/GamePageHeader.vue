ed311161ac80439706a44cdd0edeaecc9f2cc54c
ReLoia <zrennyz@gmail.com>
2024-08-23 17:19:30 +0200

    Started GamePageHeader.vue Small general fixes to the Home and css

---
+++ frontend/src/components/game/ui/GamePageHeader.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="header-info">
+    <div class="info">
+      <!--   Info about the game   -->
+      <img src="" alt="">
+      <div>
+        <h1>Game Title</h1>
+        <p>Game Description</p>
+      </div>
+    </div>
+    <div class="actions">
+      <!--   Actions -->
+      <button>
+        Add
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-info {
+  width: 100%;
+  max-width: 1400px;
+  padding-inline: 40px;
+  margin-inline: auto;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex: 1;
+
+  background: rebeccapurple;
+
+  & > div.info {
+    display: flex;
+    flex-direction: row;
+    align-items: end;
+    padding-bottom: 20px;
+    gap: 20px;
+
+    & > img {
+      display: block;
+
+      width: 200px;
+      height: 200px;
+
+      position: relative;
+    }
+  }
+
+  & > div.actions {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+  }
+}
+</style>
--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -14,7 +14,7 @@ let games = [
   {
     id: '1',
     title: 'The Witcher 3: Wild Hunt',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/0c/The_Witcher_3_cover_art.jpg',
+    image: '',
     description: 'The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. Based on The Witcher series of fantasy novels by Andrzej Sapkowski, it is the sequel to the 2011 game The Witcher 2: Assassins of Kings. Played in an open world with a third-person perspective, players control protagonist Geralt of Rivia, a monster hunter known as a witcher, who is looking for his missing adopted daughter on the run from the Wild Hunt: an otherworldly force determined to capture and use her powers.',
     likes: 0,
     rating: 10
@@ -23,85 +23,21 @@ let games = [
     id: '2',
     title: 'Cyberpunk 2077',
     image: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg',
-    description: 'Cyberpunk 2077 is a 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.',
+    description: 'Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.',
     likes: 0,
     rating: 10
   },
   {
-    id: '1',
-    title: 'The Witcher 3: Wild Hunt',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/0c/The_Witcher_3_cover_art.jpg',
+    id: '3',
+    title: 'a Hunt',
+    image: '',
     description: 'The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. Based on The Witcher series of fantasy novels by Andrzej Sapkowski, it is the sequel to the 2011 game The Witcher 2: Assassins of Kings. Played in an open world with a third-person perspective, players control protagonist Geralt of Rivia, a monster hunter known as a witcher, who is looking for his missing adopted daughter on the run from the Wild Hunt: an otherworldly force determined to capture and use her powers.',
     likes: 0,
     rating: 10
   },
   {
-    id: '2',
-    title: 'Cyberpunk 2077',
-    image: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg',
-    description: 'Cyberpunk 2077 is a 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '1',
-    title: 'The Witcher 3: Wild Hunt',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/0c/The_Witcher_3_cover_art.jpg',
-    description: 'The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. Based on The Witcher series of fantasy novels by Andrzej Sapkowski, it is the sequel to the 2011 game The Witcher 2: Assassins of Kings. Played in an open world with a third-person perspective, players control protagonist Geralt of Rivia, a monster hunter known as a witcher, who is looking for his missing adopted daughter on the run from the Wild Hunt: an otherworldly force determined to capture and use her powers.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '2',
-    title: 'Cyberpunk 2077',
-    image: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg',
-    description: 'Cyberpunk 2077 is a 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '1',
-    title: 'The Witcher 3: Wild Hunt',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/0c/The_Witcher_3_cover_art.jpg',
-    description: 'The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. Based on The Witcher series of fantasy novels by Andrzej Sapkowski, it is the sequel to the 2011 game The Witcher 2: Assassins of Kings. Played in an open world with a third-person perspective, players control protagonist Geralt of Rivia, a monster hunter known as a witcher, who is looking for his missing adopted daughter on the run from the Wild Hunt: an otherworldly force determined to capture and use her powers.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '2',
-    title: 'Cyberpunk 2077',
-    image: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg',
-    description: 'Cyberpunk 2077 is a 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '1',
-    title: 'The Witcher 3: Wild Hunt',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/0c/The_Witcher_3_cover_art.jpg',
-    description: 'The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. Based on The Witcher series of fantasy novels by Andrzej Sapkowski, it is the sequel to the 2011 game The Witcher 2: Assassins of Kings. Played in an open world with a third-person perspective, players control protagonist Geralt of Rivia, a monster hunter known as a witcher, who is looking for his missing adopted daughter on the run from the Wild Hunt: an otherworldly force determined to capture and use her powers.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '2',
-    title: 'Cyberpunk 2077',
-    image: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg',
-    description: 'Cyberpunk 2077 is a 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '1',
-    title: 'The Witcher 3: Wild Hunt',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/0c/The_Witcher_3_cover_art.jpg',
-    description: 'The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. Based on The Witcher series of fantasy novels by Andrzej Sapkowski, it is the sequel to the 2011 game The Witcher 2: Assassins of Kings. Played in an open world with a third-person perspective, players control protagonist Geralt of Rivia, a monster hunter known as a witcher, who is looking for his missing adopted daughter on the run from the Wild Hunt: an otherworldly force determined to capture and use her powers.',
-    likes: 0,
-    rating: 10
-  },
-  {
-    id: '2',
-    title: 'Cyberpunk 2077',
+    id: '4',
+    title: 'n 2077',
     image: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg',
     description: 'Cyberpunk 2077 is a 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.',
     likes: 0,
