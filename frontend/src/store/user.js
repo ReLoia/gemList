88eaeec07fcdc9807b7abd0f5c30234c5d4b5ef9
ReLoia <zrennyz@gmail.com>
2025-01-05 11:33:33 +0100

    Changed the default profile pictures

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -6,7 +6,7 @@ export const useUserStore = defineStore('user', {
         return {
             id: null,
             username: '',
-            avatar: 'https://api.dicebear.com/9.x/adventurer/png?backgroundColor=b6e3f4,c0aede,d1d4f9',
+            avatar: 'https://www.freeiconspng.com/uploads/am-a-19-year-old-multimedia-artist-student-from-manila--21.png',
             creation_date: '',
             games_rated: [],
             games_played: [],
@@ -17,6 +17,7 @@ export const useUserStore = defineStore('user', {
         setUser(userdata) {
             this.username = userdata.username;
             if (userdata.avatar) this.avatar = userdata.avatar;
+            else this.avatar = "https://api.dicebear.com/9.x/pixel-art/svg?flip=true&radius=50&backgroundType=gradientLinear,solid&randomizeIds=true&accessoriesProbability=25&beardProbability=20&clothingColor=00b159,03396c,428bca,44c585,5bc0de,88d8b0,ae0001,ffc425,ffd969,ffeead&glassesProbability=25&hatProbability=10&mouth=happy01,happy02,happy03,happy04,happy05,happy06,happy07,happy08,happy09,happy10,happy11,happy12,happy13,sad10&backgroundColor=b6e3f4,ffdfbf,aa001b,4a882a"
             this.creation_date = userdata.creation_date;
             this.games_rated = userdata.games_rated;
             this.games_played = userdata.games_played;
